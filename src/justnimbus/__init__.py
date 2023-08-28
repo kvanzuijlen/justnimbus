508e26e2b71ba54d422bf4236d0e8cd279f27ee0
kvanzuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2023-08-28 15:24:54 +0200

    Release 0.7.1

--- src/justnimbus/__init__.py
+++ src/justnimbus/__init__.py
@@ -15,10 +15,10 @@ class JustNimbusClient:
 
         try:
             response.raise_for_status()
-            return JustNimbusModel.from_dict(response.json())
+            return JustNimbusModel.from_dict(response.json()[0])
         except requests.HTTPError as error:
             if response.status_code == 404:
                 raise InvalidClientID(client_id=self._client_id) from error
             raise JustNimbusError() from error
-        except KeyError as error:
+        except LookupError as error:
             raise JustNimbusError() from error
